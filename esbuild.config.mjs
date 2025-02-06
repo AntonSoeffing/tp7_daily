37772a8f620316d9dc39fee15dbf393ecf695665
Anton Söffing <26146401+AntonSoeffing@users.noreply.github.com>
2025-02-07 00:36:00 +0100

    Reorganized into src folder

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -15,7 +15,7 @@ const context = await esbuild.context({
 	banner: {
 		js: banner,
 	},
-	entryPoints: ["main.ts"],
+	entryPoints: ["src/main.ts"],  // Changed from "main.ts" to "src/main.ts"
 	bundle: true,
 	external: [
 		"obsidian",
