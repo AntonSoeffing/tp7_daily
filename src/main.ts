168656e23d368799bf2186567d47cd7076c0336b
Anton Söffing <26146401+AntonSoeffing@users.noreply.github.com>
2025-02-07 14:36:43 +0100

    Add command and update ribbon icon for Daily Log creation in TP-7 Daily Memo

--- src/main.ts
+++ src/main.ts
@@ -5,41 +5,50 @@ import { DailyLogModal } from './DailyLogModal';
 export const VIEW_TYPE_DAILY_LOG = 'te-daily-log'; // Export the constant
 
 export default class TP7DailyMemo extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		 // Add ribbon icon
-        this.addRibbonIcon('dice', 'TP-7 Daily Memo', () => {
+        // Add command for command palette
+        this.addCommand({
+            id: 'open-tp7-daily-memo',
+            name: 'Create Daily Note from TP-7 Memos',
+            callback: () => {
+                new DailyLogModal(this.app, this).open();
+            }
+        });
+
+        // Add ribbon icon with better icon
+        this.addRibbonIcon('audio-lines', 'Create Daily Note from TP-7', () => {
             new DailyLogModal(this.app, this).open();
         });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        // Add settings tab
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+        // Using this function will automatically remove the event listener when this plugin is disabled.
+        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+            console.log('click', evt);
+        });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    }
 
-	// Removed activateTranscriberView() and activateView() or update them if needed.
-	// You may add new functions to activate the DailyLogView, if required.
+    // Removed activateTranscriberView() and activateView() or update them if needed.
+    // You may add new functions to activate the DailyLogView, if required.
 
-	onunload() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_DAILY_LOG);
-	}
+    onunload() {
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE_DAILY_LOG);
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
